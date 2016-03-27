713228f08fdba24a0e064f8d6e9a838e6f9dffea
baodoanusth <baodoan1131996@gmail.com>
2016-03-27 14:27:42 +0700

    first function

--- MayBanHang/config/routes.rb
+++ MayBanHang/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'signup'  => 'users#new' 
   resources :users
-
+  root 'users#index'
   get '/login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'

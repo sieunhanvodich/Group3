7ac3258e641873fe6485b7b81e7e4bc3b8ba6f0d
vicnoob <vic.geniusgaming@gmail.com>
2016-05-09 23:15:57 +0700

    editing html css v1

--- MayBanHang/app/controllers/application_controller.rb
+++ MayBanHang/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user 
     def current_user 
        @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+       rescue ActiveRecord::RecordNotFound => e
     end
 
     def require_user 

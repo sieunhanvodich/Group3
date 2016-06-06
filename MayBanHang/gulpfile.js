3d919f5bd32e6b2cf252174af54dbf6cc7853fd7
duong <badboy.hp96@gmail.com>
2016-06-06 21:29:14 +0700

    Added travis stuff

---
+++ MayBanHang/gulpfile.js
@@ -0,0 +1,29 @@
+// all used modules
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var runSeq = require('run-sequence');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sass = require('gulp-gulp-sass');
+var livereload = require('gulp-livereload');
+var minifyCSS = require('gulp-minify-css');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var eslint = require('gulp-eslint');
+var mocha = require('gulp-mocha');
+var karma = require('karma').server;
+var istanbul = require('gulp-istanbul');
+var notify = require('gulp-notify');
+
+// development tasks
+// ...............................................................
+
+gulp.task('travis', ['build','testSeverJS'], function() {
+	process.exit(0);
+});
+gulp.task('reload', function () {
+	livereload.reload();
+});
+
